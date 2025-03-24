473e2f4b4532578fe2a9acf124d971978d7f3092
SivaFullStackDeveloper <sivaflutterdeveloper@gmail.com>
2025-03-24 17:57:01 +0530

    functions

---
+++ function.py
@@ -0,0 +1,10 @@
+def printHelloWorld():
+    print("hello world")
+
+printHelloWorld()
+
+def helloUser():
+    name = input("enter Your Name : ")
+    print("Hello " + name +" Welcome!! ")
+
+helloUser()
---
+++ python_loops.py
@@ -0,0 +1,16 @@
+# fruits = ["banana","apple","sitaphal"]
+# for i in fruits:
+#     if i == "banana":
+#         for j in i:
+#             print(j)
+
+
+# for i in range(6):
+#     print(i)
+
+# for x in range(2,6):
+#     print(x)
+for x in range (2,30,2):
+    print(x)
+else:
+    print("finished finally")
