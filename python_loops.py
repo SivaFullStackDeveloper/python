1949278f2780148593b33668f74fe99905861731
SivaFullStackDeveloper <sivaflutterdeveloper@gmail.com>
2025-03-24 17:57:58 +0530

    functions

--- python_loops.py
+++ python_loops.py
@@ -1,15 +1,15 @@
-# fruits = ["banana","apple","sitaphal"]
-# for i in fruits:
-#     if i == "banana":
-#         for j in i:
-#             print(j)
+fruits = ["banana","apple","sitaphal"]
+for i in fruits:
+    if i == "banana":
+        for j in i:
+            print(j)
 
 
-# for i in range(6):
-#     print(i)
+for i in range(6):
+    print(i)
 
-# for x in range(2,6):
-#     print(x)
+for x in range(2,6):
+    print(x)
 for x in range (2,30,2):
     print(x)
 else:
