3609503accaecebe47041d51fc7d5bc85f2b5628
SivaFullStackDeveloper <sivaflutterdeveloper@gmail.com>
2025-03-29 19:59:07 +0530

    pandas

---
+++ panda_learning.py
@@ -0,0 +1,5 @@
+import pandas as pd
+
+file_reader = pd.read_csv("hotel-booking-data.txt")
+
+print(file_reader.head())
---
+++ pandas/panda_learning.py
@@ -0,0 +1,5 @@
+import pandas as pd
+
+file_reader = pd.read_csv("C:\\Users\\user\\Desktop\\python\\python\\pandas\\hotel-booking-data.txt",delimiter="\t")
+
+print(file_reader.head())
